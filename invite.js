1204725765334a9455608e8a881ef4b3775daf4c
Kevin Sony <kevinsony2003@gmail.com>
2023-11-02 21:21:59 -0500

    connect using nodejs

--- invite.js
+++ invite.js
@@ -1,1 +1,8 @@
-var conn=mysql.createConnection({host:"themeparkprojectdb.mysql.database.azure.com", user:"team3", password:"{your_password}", database:"{your_database}", port:3306, ssl:{ca:fs.readFileSync("{ca-cert filename}")}});
+var mysql = require('mysql');
+var fs = require('fs');
+var conn=mysql.createConnection({host:"themeparkprojectdb.mysql.database.azure.com", user:"team3", password:"Password1", database:"novapark", port:3306, ssl:{ca:fs.readFileSync("DigiCertGlobalRootCA.crt.pem")}});
+conn.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+  
