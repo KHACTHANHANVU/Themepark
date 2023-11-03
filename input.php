4482ef3e1855bab5b4b2e566de5d19602118047f
Kevin Sony <kevinsony2003@gmail.com>
2023-11-02 19:52:06 -0500

    website stackoverflww

--- input.php
+++ input.php
@@ -11,3 +11,4 @@ $CREATE = "create database if not exists novapark;";
 
 # https://storageforwebsite.z13.web.core.windows.net/
 # https://storageforwebsite-secondary.z13.web.core.windows.net/
+# https://stackoverflow.com/questions/4081981/how-to-connect-database-to-my-website
