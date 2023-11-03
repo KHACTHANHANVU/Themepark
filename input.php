4fa17ea03edf0754c3d7e3cfccfa442b0d9f700a
Kevin Sony <kevinsony2003@gmail.com>
2023-11-02 20:33:07 -0500

    example code from microsoft

--- input.php
+++ input.php
@@ -1,3 +1,4 @@
+
 <?php
 
 $host = "themeparkdbproj.database.windows.net";
@@ -5,8 +6,27 @@ $user = "team3";
 $password = "Password1"
 $conn = new mysqli($host, $user, $password);
 
+// azure database for mysql servers -> themeparkprojectdb
+
 $CREATE = "create database if not exists novapark;";
 
+?>
+
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:themeparkdbproj.database.windows.net,1433; Database = ThemePark", "team3", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "team3", "pwd" => "{your_password_here}", "Database" => "ThemePark", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:themeparkdbproj.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
 
 # https://storageforwebsite.z13.web.core.windows.net/
