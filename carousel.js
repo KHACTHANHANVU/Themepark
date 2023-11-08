73692f2f53eeaabbb7a6ef3b41906fe195b2f63b
Kevin Sony <kevinsony2003@gmail.com>
2023-11-08 17:22:13 -0600

    adding functionality

--- carousel.js
+++ carousel.js
@@ -1,5 +1,4 @@
-var database = require('./general_util.js');
-var conn = database.connection;
+/*
 
 function get_users(age) {
   const query = conn.query("SELECT first_name FROM novapark.visitor WHERE age = 18", [age], function (err, result, field) {
@@ -15,7 +14,33 @@ function get_users(age) {
 
 // Use the connection in your carousel.js
 get_users(10);
+*/
 
 // Implement your carousel functionality here
 // You can continue using the 'conn' object for database operations as needed.
+
+var general = require('./general_util.js');
+var conn = general.connection;
+var fs = require('fs');
+var http = require('http');
+var path = require('path')
+
+const hostname = '127.0.0.1';
+const port = process.env.port || 8080;
+
+fs.readFile(path.join(__dirname, '..', 'carousel.html'), function(err, file) {
+  if (err) throw err;
+
+  console.log("first\n");
+  http.createServer(function(req, res) {
+    res.statusCode = 200
+    res.write(file);
+    console.log(req.url)
+    res.end()
+
+  }).listen(port)
+  console.log("second\n");
+});
+
+
 
--- general_util.js
+++ general_util.js
@@ -1,5 +1,8 @@
 var mysql = require('mysql');
 var fs = require('fs');
+var http = require('http');
+var path = require('path')
+
 var config = {
     host:"themeparkproject.mysql.database.azure.com",
     user:"team3",
@@ -15,15 +18,12 @@ conn.connect(function(err) {
 });
 
 
-function get_users(age) {
-    query = conn.query("SELECT first_name FROM novapark.visitor WHERE (age > ?)", [age], function (err, result, field) {
+function get_visitors() {
+    query = conn.query("SELECT first_name FROM novapark.visitor", [age], function (err, result, field) {
         if (err) throw err;
         console.log(result);
     })
     conn.end()
     console.log(query)
 };
-
-// check_login_creds('team3','password')
-get_users(10)
 
