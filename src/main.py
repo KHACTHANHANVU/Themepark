f354e373a4d853656eb94a87bf34a8ccc84391ec
Kevin Sony <kevinsony2003@gmail.com>
2023-11-08 23:30:29 -0600

    adjusted html files

--- src/main.py
+++ src/main.py
@@ -2,11 +2,26 @@ import http.server
 import socketserver
 import os
 
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
 PORT = 8000
 
 class ThemeParkHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        if (self.path == '/'):
+        if (self.path == '/addingitem.html'):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            file = b""
+            try:
+                file = open("../public/addingitem.html", "rb").read()
+            finally:
+                ...
+            self.wfile.write(file)
+        elif (self.path == '/'):
             print('here')
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
@@ -18,8 +33,54 @@ class ThemeParkHandler(http.server.SimpleHTTPRequestHandler):
             finally:
                 ...
             self.wfile.write(file)
+        elif (self.path == '/connect.html'):
+            print('green')
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
 
-            
+            file = b""
+            try:
+                file = open("../public/connect.html", "rb").read()
+            finally:
+                ...
+            self.wfile.write(file)
+        elif (self.path == '/Entertainment.html'):
+            print('blue')
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            file = b""
+            try:
+                file = open("../public/Entertainment.html", "rb").read()
+            finally:
+                ...
+            self.wfile.write(file)
+        elif (self.path == '/feature.html'):
+            print('red')
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            file = b""
+            try:
+                file = open("../public/feature.html", "rb").read()
+            finally:
+                ...
+            self.wfile.write(file)
+
+        elif (self.path == '/repair%20log.html'):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            file = b""
+            try:
+                file = open("../public/repair log.html", "rb").read()
+            finally:
+                ...
+            self.wfile.write(file)
         elif (self.path == '/sales_report.html'):
             print('yellow')
             self.send_response(200)
@@ -32,15 +93,32 @@ class ThemeParkHandler(http.server.SimpleHTTPRequestHandler):
             finally:
                 ...
             self.wfile.write(file)
+        elif (self.path == '/sales_style.css'):
+            print('yellow')
+            self.send_response(200)
+            self.send_header('Content-type', 'text/css')
+            self.end_headers()
 
-        elif (self.path == '/connect.html'):
-            print('green')
-        elif (self.path == '/feature.html'):
-            print('red')
-        elif (self.path == '/Entertainment.html'):
-            print('blue')
+            file = b""
+            try:
+                file = open("../public/sales_style.css", "rb").read()
+            finally:
+                ...
+            self.wfile.write(file)
+        elif (self.path == '/sales.html'):
+            print('yellow')
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            file = b""
+            try:
+                file = open("../public/sales.html", "rb").read()
+            finally:
+                ...
+            self.wfile.write(file)
         elif (self.path == '/styles.css'):
-            print('here')
+            print('black')
             self.send_response(200)
             self.send_header('Content-type', 'text/css')
             self.end_headers()
@@ -51,6 +129,8 @@ class ThemeParkHandler(http.server.SimpleHTTPRequestHandler):
             finally:
                 ...
             self.wfile.write(file)
+        else:
+            print(self.path)
 
 
         
