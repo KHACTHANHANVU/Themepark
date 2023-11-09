ac1691e4e93f67352c5cbb34869a9f53529d4ec1
Kevin Sony <kevinsony2003@gmail.com>
2023-11-08 18:08:27 -0600

    adding more functionality

--- src/app.js
+++ src/app.js
@@ -9,19 +9,36 @@ const hostname = '127.0.0.1';
 const port = process.env.port || 8080;
 
 http.createServer(function(req, res) {
-    
-    if (req.url == "/styles.css") {
-        fs.readFile('./public/styles.css', function (err, file) {
-            if (err) throw err;
-            res.writeHead(200, {'Content-Type': 'text/css'});
-            res.write(file);
-            res.end();
-        });
-    } else if (req.url == '/') {
+    /*
+     else */
+    if (req.url == '/') {
         console.log('here')
         fs.readFile(path.join(__dirname, '..', 'public', 'carousel.html'), function (err, file) {
             if (err) {console.log(__dirname); throw err;}
             res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(file);
+            res.end();
+        });
+    } else if (req.url == '/sales_report.html') {
+        console.log('here2')
+        fs.readFile(path.join(__dirname, '..', 'public', 'sales_report.html'), function (err, file) {
+            if (err) {console.log(__dirname); throw err;}
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(file);
+            res.end();
+        });
+    } else if (req.url == '/repair%20log.html') {
+        console.log('here2')
+        fs.readFile(path.join(__dirname, '..', 'public', 'repair log.html'), function (err, file) {
+            if (err) {console.log(__dirname); throw err;}
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(file);
+            res.end();
+        });
+    }else if (req.url == "/styles.css") {
+        fs.readFile('./public/styles.css', function (err, file) {
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/css'});
             res.write(file);
             res.end();
         });
