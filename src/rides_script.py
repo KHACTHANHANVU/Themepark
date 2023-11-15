cfa558e73124cdb33e447a11ea5a79ad877e64fe
Kevin Sony <kevinsony2003@gmail.com>
2023-11-14 18:15:30 -0600

    trying to reconcile divergent branches

--- src/rides_script.py
+++ src/rides_script.py
@@ -1,28 +1,61 @@
 import mysql.connector
-from datetime import datetime
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from reportlab.platypus import Table, TableStyle
+from reportlab.lib import colors
 
 def generate_report(start_date, end_date):
-    db = mysql.connector.connect(
-        host="themeparkproject.mysql.database.azure.com",
-        user="team3",
-        password="Password1",
-        database="novapark"
-    )
+  db = mysql.connector.connect(
+    host="localhost",
+    user="yourusername",
+    password="yourpassword",
+    database="mydatabase"
+  )
+
+  cursor = db.cursor()
   
-    cursor = db.cursor()
+  # Q1: get the total number of runs for each ride, the avg number of times run daily, and wether the ride is running 
+  #query_1 = cursor.execute("SELECT ar.ride_name, SUM(dr.num_of_rides) AS total_runs, AVG(dr.num_of_rides) AS avg_num_runs, ar.is_working AS ride_operational FROM novapark.amusement_ride AS ar, novapark.daily_rides AS dr, WHERE ar.ride_no = dr.ride_no AND dr._date BETWEEN %s AND %s GROUP BY ar.ride_name", (start_date, end_date))
 
-    # reformat dates for mysql
-    start_date = start_date.strftime('%Y-%m-%d %H:%M:%S')
-    end_date = end_date.strftime('%Y-%m-%d %H:%M:%S')
+  # Q2: the number of times each ride has been repaired during the time frame and the cost of those repairs
+  #query_2 = cursor.execute("SELECT ar.ride_name, COUNT(ar.ride_no) AS repair_count, SUM(rr.cost) AS total_repair_cost FROM navapark.amusement_ride AS ar, novapark.ride_repair AS rr WHERE ar.ride_no = rr.ride_no AND rr.date_of_issue >= %s AND rr.repair_date <= %s GROUP BY ar.ride_name", (start_date, end_date))
 
-    # Q1: get the total number of riders for each ride
-    cursor.execute("SELECT ar.ride_name, SUM(dr.num_of_rides) AS total_riders FROM novapark.amusement_ride AS ar, novapark.daily_rides AS dr WHERE ar.ride_no = dr.ride_no AND dr._date BETWEEN %s AND %s", (start_date, end_date))
-
-    # Q2: get the average number of daily riders per ride
-    cursor.execute("SELECT ar.ride_name, AVG(dr.num_of_rides) AS avg_daily_riders FROM novapark.amusement_ride AS ar, novapark.daily_rides AS dr WHERE ar.ride_no = dr.ride_no AND dr._date BETWEEN %s AND %s", (start_date, end_date))
-
-    # Q3: the number of times each ride has been repaired
-    cursor.execute("SELECT ar.ride_name, COUNT(ar.ride_no) AS repair_count FROM navapark.amusement_ride AS ar, novapark.ride_repair AS rr WHERE ar.ride_no = rr.ride_no AND rr.repair_date BETWEEN %s AND %s", (start_date, end_date))
-
-    # Q4: total cost of repairing each ride
-    cursor.execute("SELECT ar.ride_name, SUM(rr.cost) AS total_repair_cost FROM navapark.amusement_ride AS ar, novapark.ride_repair AS rr WHERE ar.ride_no = rr.ride_no AND rr.repair_date BETWEEN %s AND %s", (start_date, end_date))
+  # combined queries 1 and 2
+  query = cursor.execute("""SELECT ar.ride_name,
+         SUM(dr.num_of_rides) AS total_runs,
+         AVG(dr.num_of_rides) AS avg_num_runs,
+         IF(ar.is_working, "Yes", "No") AS ride_operational,
+         COUNT(rr.ride_no) AS repair_count,
+         SUM(rr.cost) AS total_repair_cost
+  FROM novapark.amusement_ride AS ar
+  LEFT JOIN novapark.daily_rides AS dr ON ar.ride_no = dr.ride_no AND dr._date BETWEEN %s AND %s
+  LEFT JOIN novapark.ride_repair AS rr ON ar.ride_no = rr.ride_no AND rr.date_of_issue >= %s AND rr.repair_date <= %s
+  GROUP BY ar.ride_name""")
+  
+  c = canvas.Canvas("rides_report.pdf", pagesize=letter)
+  c.setFont("Helvetica", 12)
+  
+  # Add table data (including headers)
+  headers = ('Ride Name', 'Total Rides', 'Average Daily Riders', 'Number of Repairs', 'Total Cost of Repairs', 'Ride Operational')
+  data = [headers]  
+  
+  # Extend data with rows of actual data (formatted as tuples)
+  data.extend(query)
+  
+  # Create a table style
+  table_style = TableStyle([
+      ('BACKGROUND', (0, 0), (-1, 0), colors.grey),  
+      ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),  
+      ('ALIGN', (0, 0), (-1, -1), 'CENTER'),  
+      ('GRID', (0, 0), (-1, -1), 1, colors.black),  
+  ])
+  
+  # Create the table
+  table = Table(data)
+  table.setStyle(table_style)
+  
+  table.wrapOn(c, 400, 600)
+  table.drawOn(c, 50, 700)  
+  
+  # Save the PDF file if needed, replace with code to diplay if needed
+  # c.save()
--- src/staff_script.py
+++ src/staff_script.py
@@ -15,10 +15,40 @@ def generate_report(dept_name, fname, lname, min_wage, max_wage):
     query = ""
     if(fname == "" or lname == ""):
       # Q1: wihtout name
-      query = cursor.execute("SELECT s.fname AS first_name, s.lname AS last_name, s.staff_no AS staff_number, s.address, s.week_wage AS weekly_wage, s.job, d.d_name AS dept_name, d.d_no AS dept_num FROM novapark.department AS d, novapark.staff AS s WHERE d.d_name == %s AND d.d_no == s.d_no AND s.week_wage BETWEEN %s AND %s GROUP BY d.d_name;", (dept_name, min_wage, max_wage))
+      query = cursor.execute("SELECT s.fname AS first_name, s.lname AS last_name, s.staff_no AS staff_number, s.address, s.week_wage AS weekly_wage, s.job, d.d_name AS dept_name, d.d_no AS dept_num FROM novapark.department AS d, novapark.staff AS s WHERE d.d_name == %s AND d.d_no == s.d_no AND s.week_wage BETWEEN %s AND %s GROUP BY d.d_name", (dept_name, min_wage, max_wage))
     elif(min_wage == 0 and max_wage == 0):
       # Q2: wihtout wage
-      query = cursor.execute("SELECT s.fname AS first_name, s.lname AS last_name, s.staff_no AS staff_number, s.address, s.job, d.d_name AS dept_name, d.d_no AS dept_num FROM novapark.department AS d, novapark.staff AS s WHERE d.d_name == %s AND d.d_no == s.d_no AND s.fname == %s AND s.lname == %s GROUP BY d.d_name;", (dept_name, fname, lname))
+      query = cursor.execute("SELECT s.fname AS first_name, s.lname AS last_name, s.staff_no AS staff_number, s.address, s.job, d.d_name AS dept_name, d.d_no AS dept_num FROM novapark.department AS d, novapark.staff AS s WHERE d.d_name == %s AND d.d_no == s.d_no AND s.fname == %s AND s.lname == %s GROUP BY d.d_name", (dept_name, fname, lname))
     else:
       # Q3: all params
-      query = cursor.execute("SELECT s.fname AS first_name, s.lname AS last_name, s.staff_no AS staff_number, s.address, s.week_wage AS weekly_wage, s.job, d.d_name AS dept_name, d.d_no AS dept_num FROM novapark.department AS d, novapark.staff AS s WHERE d.d_name == %s AND d.d_no == s.d_no AND s.fname == %s AND s.lname == %s AND s.week_wage BETWEEN %s AND %s GROUP BY d.d_name;", (dept_name, fname, lname, min_wage, max_wage))
+      query = cursor.execute("SELECT s.fname AS first_name, s.lname AS last_name, s.staff_no AS staff_number, s.address, s.week_wage AS weekly_wage, s.job, d.d_name AS dept_name, d.d_no AS dept_num FROM novapark.department AS d, novapark.staff AS s WHERE d.d_name == %s AND d.d_no == s.d_no AND s.fname == %s AND s.lname == %s AND s.week_wage BETWEEN %s AND %s GROUP BY d.d_name", (dept_name, fname, lname, min_wage, max_wage))
+    
+    # create canvas and report file
+    c = canvas.Canvas("staff_report.pdf", pagesize=letter)
+    c.setFont("Helvetica", 12)
+
+    # Add table data (including headers)
+    headers = ('Ride Name', 'Total Rides', 'Average Daily Riders', 'Number of Repairs', 'Total Cost of Repairs')
+    data = [headers]  
+
+    # Extend data with rows of actual data (formatted as tuples)
+    data.extend(query)
+
+    # Create a table style
+    table_style = TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),  
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),  
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),  
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),  
+    ])
+
+    # Create the table
+    table = Table(data)
+    table.setStyle(table_style)
+
+    # put table on canvas
+    table.wrapOn(c, 400, 600)
+    table.drawOn(c, 72, 700)  
+
+    # Save the PDF file if needed, or replace with the code to display it
+    # c.save()
