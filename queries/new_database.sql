4fd42964f747ba3323a5dec84909fb4d99b41980
Connor <mrcman71@gmail.com>
2023-11-21 14:19:23 -0600

    changed schema

--- queries/new_database.sql
+++ queries/new_database.sql
@@ -9,14 +9,20 @@ CREATE TABLE novapark.staff (
     phone_no CHAR(10),
     addrs VARCHAR(35),
     supervisor_id SMALLINT,
-    hours_work CHAR(3),
     hourly_wage NUMERIC(8,2) NOT NULL CHECK(hourly_wage > 7.25),
     dob DATE NOT NULL,
     job ENUM('manager', 'repair', 'rides') NOT,
 );
 
 ALTER TABLE novapark.staff ADD FOREIGN KEY (supervisor_id) REFERENCES novapark.staff(staff_id);
-ALTER TABLE novapark.staff AUTO_INCREMENT=100;
+ALTER TABLE novapark.staff AUTO_INCREMENT=1;
+
+CREATE TABLE novapark.hours_worked { 
+    staff_id SMALLINT,
+    num_hours SMALLINT,
+    cur_date DATE
+    PRIMARY KEY (staff_id, cur_date)
+};
 
 CREATE TABLE novapark.customer (
     first_name VARCHAR(30),
@@ -24,23 +30,16 @@ CREATE TABLE novapark.customer (
     pswrd VARCHAR(10),
     email VARCHAR(35),
     phone CHAR(10),
-    pass_credits SMALLINT,
+    pass_credits SMALLINT DEFAULT 0,
     num_passes INT DEFAULT 0,
-    card_num VARCHAR(16),
-    cvv VARCHAR(3),
-    exp_month VARCHAR(2),
-    exp_year VARCHAR(2),
     PRIMARY KEY (email)
-    # FOREIGN KEY (email) REFERENCES novapark.park_pass(cust_email)
 );
-
-
-ALTER TABLE novapark.customer AUTO_INCREMENT=100;
 
 CREATE TABLE novapark.park_pass (
     cust_email VARCHAR(35),
     num_passes INT,
-    pass_type ENUM('daily', 'weekly', 'montly'),
+    sale_cost INT,
+    pass_type ENUM('Silver', 'Gold', 'Platinum'),
     date_bought DATETIME,
     card_fname VARCHAR(30),
     card_lname VARCHAR(30),
@@ -48,7 +47,7 @@ CREATE TABLE novapark.park_pass (
     cvv VARCHAR(3),
     exp_month VARCHAR(2),
     exp_year VARCHAR(2)
-    PRIMARY KEY (cust_email, pass_type, date_bought)
+    PRIMARY KEY (cust_email, date_bought)
 );
 
 
@@ -58,7 +57,6 @@ CREATE TABLE novapark.amusement_ride (
     is_working BOOL DEFAULT TRUE,
     date_of_last_repair DATETIME,
     PRIMARY KEY (ride_no)
-    # FOREIGN KEY (ride_no) REFERENCES novapark.ride_repair(ride_no)
 );
 
 ALTER TABLE novapark.amusement_ride ADD FOREIGN KEY (ride_no) REFERENCES novapark.ride_repair(ride_no);
@@ -80,10 +78,7 @@ CREATE TABLE novapark.events (
 );
 
 CREATE TABLE novapark.business_day (
-	num_of_visitors SMALLINT NOT NULL CHECK(num_of_visitors > -1),
     b_date DATE PRIMARY KEY,
-    revenue NUMERIC(8,2) NOT NULL CHECK (revenue > 0.0),
-    expenses NUMERIC(8,2) NOT NULL CHECK (expenses > 0.0)    
+    revenue FLOAT NOT NULL CHECK (revenue > 0.0),
+    expenses FLOAT NOT NULL CHECK (expenses > 0.0)
 );
-
-
--- queries/triggers.sql
+++ queries/triggers.sql
@@ -13,7 +13,7 @@ BEGIN
     IF DATEDIFF(NEW.date_bought, last_pass_credit_date) >= 30 THEN
 
         SELECT park.num_passes INTO total_passes
-        FROM novapark.park_pass AS park 
+        FROM novapark.park_pass AS park
         WHERE NEW.cust_email = park.cust_email AND park.date_bought BETWEEN last_pass_credit_date AND NEW.date_bought;
 
         IF (total_passes + NEW.num_passes) > 10 THEN
@@ -24,55 +24,3 @@ BEGIN
     END IF;
 END //
 DELIMITER ;
-
-DELIMITER //
-CREATE TRIGGER trigger_Employee_inserthour
-    BEFORE UPDATE ON novapark.staff
-    FOR EACH ROW
-BEGIN
-    DECLARE rowCount INT;
-    
-    SELECT COUNT(*) INTO rowCount FROM novapark.staff;
-    
-    IF rowCount = 0 THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Table Staff does not have any data';
-    END IF;
-
-    IF NEW.hours_work > 40 THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Table staff cannot be updated';
-    END IF;
-    
-    IF NEW.hours_work > 40 THEN
-        UPDATE week_wage
-        SET week_wage = week_wage * 1.5 * NEW.hours_work
-        WHERE staff_id = NEW.staff_id;
-    END IF;
-END //
-DELIMITER ;
-
-
-DELIMITER //
-CREATE TRIGGER trigger_visitor_ispresent
-    BEFORE UPDATE ON novapark.visitor
-    FOR EACH ROW
-BEGIN
-    DECLARE rowCount INT;
-    
-    SELECT COUNT(*) INTO rowCount FROM novapark.visitor;
-    
-    IF rowCount = 0 THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Table Visitor does not have any data';
-    END IF;
-
-    IF NEW.ticket_no IS NOT NULL THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'You are qualified for coupons';
-    END IF;
-    
-    IF NEW.is_present = TRUE THEN
-        UPDATE novapark.price
-        SET price = price * 0.75;
-    END IF;
-END;
-//
-DELIMITER ;
-*/
