1330eb2af0627f2c0a35ae95c6d06e1dda8544b2
Connor <mrcman71@gmail.com>
2023-11-24 22:38:27 -0600

    trigger 1

--- queries/triggers.sql
+++ queries/triggers.sql
@@ -4,22 +4,22 @@ CREATE TRIGGER trigger_on_pass_purchase
     BEFORE INSERT ON novapark.park_pass
     FOR EACH ROW
 BEGIN
-    DECLARE last_pass_credit_date DATE;
+    DECLARE last_credit_date DATE;
     DECLARE total_passes SMALLINT;
 
-    SELECT cs.last_credit_date INTO last_pass_credit_date
+    SELECT cs.last_pass_credit_date INTO last_credit_date
     FROM novapark.customer AS cs
     WHERE NEW.cust_email = cs.email;
 
-    IF (DATEDIFF(NEW.date_bought, last_pass_credit_date) >= 30 OR last_pass_credit_date IS NULL) THEN
+    IF (DATEDIFF(NEW.date_bought, last_credit_date) >= 30 OR last_credit_date IS NULL) THEN
 
         SELECT COALESCE(SUM(park.num_passes), 0) INTO total_passes
         FROM novapark.park_pass AS park
-        WHERE NEW.cust_email = park.cust_email AND park.date_bought BETWEEN last_pass_credit_date AND NEW.date_bought;
+        WHERE NEW.cust_email = park.cust_email AND park.date_bought BETWEEN last_credit_date AND NEW.date_bought;
 
         IF (total_passes + NEW.num_passes) > 10 THEN
             UPDATE novapark.customer AS cs
-            SET cs.last_credit_date = NEW.date_bought, cs.pass_credits = cs.pass_credits + 10 
+            SET cs.last_pass_credit_date = NEW.date_bought, cs.pass_credits = cs.pass_credits + 10 
             WHERE NEW.cust_email = cs.email;
         END IF;
     END IF;
